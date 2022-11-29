27ff000c06c0fca3fd1dac66702674330ba92d50
next <next@qq.com>
2022-11-29 13:49:32 +0800

    sbuject 4

--- main.c
+++ main.c
@@ -1,12 +1,25 @@
-/*P29 练一练*/
-#include<stdio.h>
+/*P33 练一练*/
+#include <stdio.h>
+//圆周率 宏定义
+#define PI 3.14
+//计算次方
+float Power(float sou,int multi)
+{
+	float result = 1;
+	for (int i = 0; i < multi; i++)
+	{
+		result *= sou;
+	}
+	return result;
+}
 //程序入口
 int main()
 {
-	char cIndex;
-	printf("请输入一个字符_\n");//提示
-	scanf("%c", &cIndex);
-	printf("%c 的ASCll码值为 = %d\n", cIndex,cIndex);
-	
+	float rVolume,radius;
+	printf("请输入球的半径_\n");
+	scanf("%f", &radius);
+	printf("球的半径 = %0.2f\n", radius);
+	rVolume = 4 / 3 * (float)PI * Power(radius, 3);
+	printf("球的体积V = %0.2f\n", rVolume);
 	return 0;
 }
