3cc3d44ad8246f043d1d3f6e298c42f8c25fcf2e
next <next@qq.com>
2022-12-01 16:21:59 +0800

    subject 3

--- main.c
+++ main.c
@@ -1,43 +1,28 @@
-//P66习题三 2
+//P66习题三 4
 #include<stdio.h>
-#define TRUE 1
-#define FALSE 0
-//入口函数
-int main()
-{
+#include<math.h>
 
-	int length;//指定数组长度3
-	printf("输入你要的整数个数_\n");
-	scanf("%d", &length);
-	//存储数值
-	int inNumbs[length];
-	printf("请输入%d个整数并以空格开_\n", length);
-	//等待输入完指定的整数个数
-	for (int i = 0; i < length; i++)
-		scanf("%d", &inNumbs[i]);
-	//遍历数组
-	for (int n = 0; n < length - 1; n++)
-	{
-		int isChanged = FALSE;
-		//排序
-		for (int j = 0; j < length - 1; j++) 
-		{
-			if (inNumbs[j] > inNumbs[j + 1]) 
-			{
-				int temp = inNumbs[j];
-				inNumbs[j] = inNumbs[j + 1];
-				inNumbs[j + 1] = temp;
-				isChanged = TRUE; //发生变换位置
-			}
-		}
-		if (isChanged == FALSE) {
-			break;
-		}
+//入口函数
+int main() {
+
+	float a, b, c; //边
+	printf("输入三条边组成三角形以空格开_\n");
+	scanf("%f%f%f", &a, &b, &c);
+	printf("a = %0.2f b = %0.2f c = %0.2f\n", a, b, c);
+
+	if (a + b > c && a + c > b && b + c > a) {
+		printf("可以组成三角行\n");
+		float peri = a + b + c;
+
+		float temp = peri / 2.0f;
+		float area = sqrt(temp * (temp - a) * (temp - b) * (temp - c)); //面积
+		
+		printf("三角形的周长 = %0.2f\n三角形的面积 = %0.2f",peri,area);
+
 	}
-	printf("\n从小到大排序\n");
-	for (int t = 0; t < length; t++) 
+	else
 	{
-		printf("%d ",inNumbs[t]);//输出
+		printf("不能组成三角形\n");
 	}
 
 	return 0;
