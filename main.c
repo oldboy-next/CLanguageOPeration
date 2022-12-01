cbb9c653f5e50c41e4f8316b9dbce125ef046c1b
next <next@qq.com>
2022-12-01 13:55:33 +0800

    subject 1

--- main.c
+++ main.c
@@ -1,10 +1,22 @@
+//P66习题三 1
 #include<stdio.h>
 
-//运行框架
-int main()
-{
-	//use githup
-	printf("\nHello World!\tRun Success!\n");
-	
+int main() {
+	int iYear;
+	printf("请输入一个年份year：\n");
+	scanf("%d", &iYear);
+
+	if ((iYear % 4 == 0 && iYear % 100 != 0) || iYear % 400 == 0) {
+		if(iYear%400==0){
+			printf("\n%d是世纪闰年\n",iYear);
+			return 0;
+		}
+		printf("\n%d是普通闰年\n",iYear);
+	}
+	else
+	{
+		printf("\n%d不是闰年\n",iYear);
+	}
+
 	return 0;
 }
